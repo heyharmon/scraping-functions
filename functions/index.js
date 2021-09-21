cfaec281eeb960d4f3ef3c6fabb74b2e01102464
Ryan Harmon <heyharmon@gmail.com>
2021-09-20 21:25:45 -0600

    Setup cors config as const

--- functions/index.js
+++ functions/index.js
@@ -2,12 +2,16 @@ const functions = require('firebase-functions')
 const express = require('express')
 const cors = require('cors')
 
-// Import routes
+const corsConfig = {
+    methods: ['GET'],
+    origin: true
+}
+
 const routes = require('./routes.js')
 
 // Setup Express app
 const app = express()
-      app.use(cors({origin: true}))
+      app.use(cors(corsConfig))
       app.use('/', routes)
 
 // Return Express app from Firebase Function on "/api"
